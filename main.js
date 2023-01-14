1462859830e849d759a3f9d537cc6bfb04c7c1e0
Jessica Nash <jessicanashacc@gmail.com>
2023-01-14 21:21:26 +0900

    field print method complete

--- main.js
+++ main.js
@@ -11,9 +11,29 @@ class Field {
   }
 
   print() {
-    console.log(field.join())
+    // needs to print in columns
+    let fieldString = '' //initialize the field string (empty for now)
+
+    for (let i=0; i < this.field.length; i++) {  // iterate through the array of arrays
+      this.field[i].forEach(e => fieldString += e);  // iterate through each array to add each element to the field string
+      fieldString += '\n'; // \n <-- special characters denoting the start of a new line
+    }
+    console.log(fieldString);
   }
+
+
+// Methods to test whether the current location results in win (user is on the hat) or a loss (user is on a hole or out-of-bounds).
+// A method to handle asking and accepting user input, and updating the current location.
+// A method to run the main game loop until the game is won or lost.
 }
 
-const name = prompt('What is your name?');
-console.log(`Hey there ${name}`);
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
+
+const direction = prompt('Which way?');
+console.log(`You chose ${direction}`);
